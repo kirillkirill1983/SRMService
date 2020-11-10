75802b4f928d5aea2a7413f3f8e54448963c1511
kirillkirill1983 <baevkirill1983@gmail.com>
2020-11-10 09:21:46 +0200

    no message

--- ServiceStationApi.Business/ICustomerBusiness.cs
+++ ServiceStationApi.Business/ICustomerBusiness.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ServiceStationApi.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ServiceStationApi.Business
 {
-    class ICustomerbusiness
+     public interface ICustomerBusiness
     {
+        Task<bool> AddAsync(Customer customer);
     }
 }
